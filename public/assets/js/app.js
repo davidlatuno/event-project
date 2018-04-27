186d56d17efb7af0c03fb7f0dea8056771a0843b
davidlatuno <dslatuno@gmail.com>
2018-04-27 12:38:32 -0700

    Tested client side api

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,16 +1,20 @@
 $(".keywordButtons").on("click", "button", function (event) {
+    var api = $(this).attr("data-api");
     var keyword = {
         event: $(this).attr("data-event")
     }
-    $.post("/yelp", keyword).then(function (data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            $(".eventInfo").append("<p>" + data[i].name + "</p>")
-            $(".eventInfo").append("<p>" + data[i].phone + "</p>")
-            $(".eventInfo").append("<p><a href=" +data[i].url + ">Yelp Link</a></p>")
-            $(".eventInfo").append("<p>" + data[i].location.address1 + "</p>")
-        }
-        
-        // location.reload();
-    })
+    if (api === "yelp") {
+        $.post("/yelp", keyword).then(function (data) {
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                $(".eventInfo").append("<p>" + data[i].name + "</p>")
+                $(".eventInfo").append("<p>" + data[i].phone + "</p>")
+                $(".eventInfo").append("<p><a href=" + data[i].url + ">Yelp Link</a></p>")
+                $(".eventInfo").append("<p>" + data[i].location.address1 + "</p>")
+            }
+
+            // location.reload();
+        })
+    }
+
 })

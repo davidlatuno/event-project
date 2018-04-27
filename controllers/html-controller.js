4b635daada4136caaee62128ebc56598e683744f
nkand84 <nityabhagi@gmail.com>
2018-04-27 09:55:53 -0700

    CSS changes

--- controllers/html-controller.js
+++ controllers/html-controller.js
@@ -2,8 +2,20 @@ var express = require("express");
 var router = express.Router();
 var path = require("path");
 
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/index.html"));
+});
+router.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/login.html"));
+});
+router.get("/signup", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/signup.html"));
+});
+router.get("/profile", function(req, res) {
+    res.render(path.join(__dirname, "../views/profile"));
+});
 router.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../views/index.html"));
-})
+});
 
 module.exports = router;

828e65987f69310df073ffb74651be02612a61a8
nkand84 <nityabhagi@gmail.com>
2018-04-27 11:22:21 -0700

    minor changes to html files

--- controllers/html-controller.js
+++ controllers/html-controller.js
@@ -2,29 +2,18 @@ var express = require("express");
 var router = express.Router();
 var path = require("path");
 
-<<<<<<< HEAD
-router.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/index.html"));
-});
-router.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/login.html"));
-});
 router.get("/signup", function(req, res) {
     res.sendFile(path.join(__dirname, "../views/signup.html"));
-});
+})
+
+router.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/login.html"));
+})
+
 router.get("/profile", function(req, res) {
     res.render(path.join(__dirname, "../views/profile"));
 });
-=======
-router.get("/signup", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/signup.html"));
-})
 
-router.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/login.html"));
-})
-
->>>>>>> 0cbd261449e04b3fbcb85a53812369ebdbdba774
 router.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../views/index.html"));
 });

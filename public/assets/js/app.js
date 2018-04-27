574a6acc9bc48cdac7e702fab8c93320aa1dcf02
davidlatuno <dslatuno@gmail.com>
2018-04-27 13:31:57 -0700

    Added functionality for yelp and meetup, changed some html for profile page

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,8 +4,8 @@ $(".keywordButtons").on("click", "button", function (event) {
         event: $(this).attr("data-event")
     }
     if (api === "yelp") {
+        $(".eventInfo").empty();
         $.post("/yelp", keyword).then(function (data) {
-            console.log(data);
             for (var i = 0; i < data.length; i++) {
                 $(".eventInfo").append("<p>" + data[i].name + "</p>")
                 $(".eventInfo").append("<p>" + data[i].phone + "</p>")
@@ -14,6 +14,21 @@ $(".keywordButtons").on("click", "button", function (event) {
             }
 
             // location.reload();
+        })
+    }
+
+    if(api === "meetup") {
+        $(".eventInfo").empty();
+        $.post("/meetup", keyword).then(function(data) {
+            var body = data
+            for (var i = 0; i < body.length; i++) {
+                // console.log(body);
+                $(".eventInfo").append("<p>" + body[i].name + "</p>");
+                $(".eventInfo").append("<p>" + body[i].link + "</p>");
+                $(".eventInfo").append("<p>" + body[i].status + "</p>");
+                $(".eventInfo").append(body[i].description);
+                $(".eventInfo").append("-------------------")
+            }
         })
     }
 

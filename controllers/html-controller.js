645f67fce950f538be702ef9eaaf0eb5c25676a0
Johana Ramirez <JCRamirez@Johanas-MacBook-Pro.local>
2018-04-27 12:04:03 -0700

    recent changes

--- controllers/html-controller.js
+++ controllers/html-controller.js
@@ -2,20 +2,6 @@ var express = require("express");
 var router = express.Router();
 var path = require("path");
 
-<<<<<<< HEAD
-router.get("/signup", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/signup.html"));
-})
-
-router.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/login.html"));
-})
-
-router.get("/profile", function(req, res) {
-    res.render(path.join(__dirname, "../views/profile"));
-})
-
-=======
 router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../views/index.html"));
 });
@@ -28,7 +14,6 @@ router.get("/signup", function(req, res) {
 router.get("/profile", function(req, res) {
     res.render(path.join(__dirname, "../views/profile"));
 });
->>>>>>> 1b03bcb3ca5b0850c49af8ace0a7e6f422fd955a
 router.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../views/index.html"));
 });
